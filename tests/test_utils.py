e15d91a25186daaa55c4d47d694beb5685b61c59
Douglas Treadwell <douglas.treadwell@gmail.com>
2017-06-17 22:22:41 -0700

    Adding utility functions from marshmallow to apispec.
    If apispec is intended to be used with schema libraries
    other than marshmallow, and apispec itself doesn't
    depend on marshmallow, this must be done to avoid
    users of this library needing to install marshmallow
    only for the few marshmallow utilities used by apispec.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from apispec import utils
 
+
 def test_load_yaml_from_docstring():
     def f():
         """
@@ -14,3 +15,10 @@ def test_load_yaml_from_docstring():
         """
     result = utils.load_yaml_from_docstring(f.__doc__)
     assert result == {'herp': 1, 'derp': 2}
+
+
+# from marshmallow.utils
+def test_is_collection():
+    assert utils.is_collection([1, 'foo', {}]) is True
+    assert utils.is_collection(('foo', 2.3)) is True
+    assert utils.is_collection({'foo': 'bar'}) is False
